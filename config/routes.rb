52b2e08fb2ff6c78ed7906a32f418c34b86fdc3d
daiaventureira <daiane.souzas@icloud.com>
2021-02-23 00:25:21 -0500

    out the orute

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   resources :notes
-  get "/", to: "notes#index"
   mount ActionCable.server => '/cable'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
