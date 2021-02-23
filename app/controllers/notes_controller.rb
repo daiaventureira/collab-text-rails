7cb917004a0cfdaf97c7c500e7628b771397894c
daiaventureira <daiane.souzas@icloud.com>
2021-02-23 00:07:14 -0500

    added cors

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -8,6 +8,13 @@ class NotesController < ApplicationController
     render json: @notes
   end
 
+  def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  end
+
   # GET /notes/1
   def show
     render json: @note
