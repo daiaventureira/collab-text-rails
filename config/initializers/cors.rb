24025f51efd0814c512ab7b88184dbf96f1f09a6
daiaventureira <daiane.souzas@icloud.com>
2021-04-19 11:02:32 -0400

    changed cors.rb origins to *

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://daiane.codes/collab-text-js/'
+    origins '*'
 
     resource '*',
       headers: :any,
