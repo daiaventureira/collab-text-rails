ab8072ac78f5f56cc2103f37aa35b5e1847983fa
daiaventureira <daiane.souzas@icloud.com>
2021-04-19 11:15:40 -0400

    chenged action cable config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,5 +63,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   config.action_cable.disable_request_forgery_protection = true
-  config.active_job.queu_adapter = :sidekiq
+  # config.active_job.queu_adapter = :sidekiq
+  config.action_cable.url = 'ws://https://collab-text.herokuapp.com/notes//cable'
+  config.action_cable.allowed_request_origins = ['https://daiane.codes/collab-text-js/']
 end
